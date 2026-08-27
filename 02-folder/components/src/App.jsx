2a3567fbc24bf790a30e0c469d2e6261b5e19c53
Kamineni Jeswanth Sriram <jeswanthkamineni@gmail.com>
2026-08-27 10:16:57 +0530

    update

---
+++ 02-folder/components/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar.jsx";
+import Card from "./components/Card.jsx"
+
+import React from 'react'
+
+const App = () => {
+  return (
+      <div>
+         <Navbar/>
+         <Card/>
+      </div>
+  )
+}
+
+export default App
---
+++ 02-folder/components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+        <h1>Ram</h1>
+      
+    </div>
+  )
+}
+
+export default Card
